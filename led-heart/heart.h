f9a46697e86de1e150a39a9896e4d22d11907ea3
Nikolas Coukouma <atrus@atrus.org>
2010-02-07 18:43:41 -0500

    refactor some more. try (and fail) to setup sleep mode

--- led-heart/heart.h
+++ led-heart/heart.h
@@ -2,13 +2,15 @@
 
 #define LED_DEF(pi_pos, pi_gnd) \
     { \
-      _BV(pi_pos), _BV(pi_gnd) \
+      pi_pos, pi_gnd \
     }
 #define LED_PAIR(pi_pos, pi_gnd) \
     LED_DEF(pi_pos, pi_gnd), \
     LED_DEF(pi_gnd, pi_pos)
 
-static const struct {uint8_t pos, gnd;}
+typedef struct {uint8_t pos, gnd;} led_pair_t;
+
+static const led_pair_t
   LED_PINS[] = {
     LED_PAIR(5, 4),
     LED_PAIR(5, 3),
@@ -26,3 +28,41 @@ static const struct {uint8_t pos, gnd;}
     LED_DEF(2, 0),
   };
 static const uint8_t NUM_LEDS = 27;
+
+#define POWER_PIN PINB
+static const uint8_t POWER_BIT = 2;
+
+static const uint8_t ALL_INPUT = 0, ALL_OUTPUT = ~0,
+  ALL_POS_PULL_ON = ~0, ALL_GND_PULL_OFF = 0;
+
+static inline void set_output(uint8_t *ddr, uint8_t pin_no) {
+  *ddr |= _BV(pin_no);
+}
+
+static inline void set_input(uint8_t *ddr, uint8_t pin_no) {
+  *ddr &= ~_BV(pin_no);
+}
+
+static inline void set_pos(uint8_t *port, uint8_t pin_no) {
+  set_output(port, pin_no);
+}
+
+static inline void set_gnd(uint8_t *port, uint8_t pin_no) {
+  set_input(port, pin_no);
+}
+
+static inline void set_pull_on(uint8_t *port, uint8_t pin_no) {
+  set_output(port, pin_no);
+}
+
+static inline void set_pull_off(uint8_t *port, uint8_t pin_no) {
+  set_input(port, pin_no);
+}
+
+static inline void turn_led_on(uint8_t *ddr, uint8_t *port, uint8_t num) {
+  set_output(ddr, LED_PINS[num].pos);
+  set_output(ddr, LED_PINS[num].gnd);
+  set_pos(port, LED_PINS[num].pos);
+  set_gnd(port, LED_PINS[num].gnd);
+}
+
--- led-heart/shine-all.c
+++ led-heart/shine-all.c
@@ -1,15 +1,53 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
 #include "heart.h"
 
+static const uint32_t DEBOUNCE_POWER_LOOPS = F_CPU/150;
+
+//EMPTY_INTERRUPT(EXT_INT0_vect);
+ISR(EXT_INT0_vect){}
+
 int main() {
+  DDRB = ALL_INPUT;
+  PORTB = ALL_POS_PULL_ON;
+  DDRA = ALL_INPUT;
+  PORTA = ALL_GND_PULL_OFF;
+  cli();
   while (1) {
     for(uint8_t i = 0; i < 27; i++) {
-      static uint8_t tmp_port = 0, tmp_ddr = 0;
-      tmp_ddr = LED_PINS[i].pos | LED_PINS[i].gnd;
-      tmp_port = LED_PINS[i].pos;
+      uint8_t tmp_port = ALL_GND_PULL_OFF, tmp_ddr = ALL_INPUT;
+      turn_led_on(&tmp_ddr, &tmp_port, i);
       DDRA = tmp_ddr;
       PORTA = tmp_port;
     }
+    DDRA = ALL_INPUT;
+    PORTA = ALL_GND_PULL_OFF;
+    if (bit_is_clear(POWER_PIN, POWER_BIT)) {
+      _delay_ms(100);
+      if (bit_is_clear(POWER_PIN, POWER_BIT)) {
+        // we really just want to go into power down mode, and be woken up when
+        // someone hits the "power" button
+        _delay_ms(500);
+        // this pin change interrupt business is inspired by the Game of Life's
+        // super wacky code. INT0 should work for us, right???
+        PCMSK1 |= _BV(PCINT10);
+        MCUCR |= _BV(ISC01);
+        GIMSK |= _BV(INT0) | _BV(PCIE1);
+        sleep_enable();
+        //set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+        set_sleep_mode(SLEEP_MODE_IDLE);
+        sei();
+        sleep_cpu();
+        sleep_disable();
+        cli();
+        _delay_ms(500);
+      }
+    }
   }
+
+  DDRA = ALL_INPUT;
+  PORTA = ALL_GND_PULL_OFF;
   return 0;
 }
